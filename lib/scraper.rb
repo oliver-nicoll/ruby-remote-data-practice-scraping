4c1762dc14bccd2c963569bb31b5a2d73a874881
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-18 20:51:26 -0500

    commit tree ceaaf9de1542b0c33cc8f561dee72c3e1c1f25a2

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+
+
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4")
+
+# doc.css(".site-header__hero__headline").text.strip
+
+# doc.css(".headline-260IBN").text
